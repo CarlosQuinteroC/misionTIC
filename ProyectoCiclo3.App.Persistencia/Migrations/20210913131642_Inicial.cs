626186b6c2b41d0f4aca82a01458483c19036a7b
CarlosQuinteroC <chqc11@gmail.com>
2021-09-20 09:36:23 -0500

    First Commit.

---
+++ ProyectoCiclo3.App.Persistencia/Migrations/20210913131642_Inicial.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ProyectoCiclo3.App.Persistencia.Migrations
+{
+    public partial class Inicial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Aviones",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    marca = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    modelo = table.Column<int>(type: "int", nullable: false),
+                    numero_asientos = table.Column<int>(type: "int", nullable: false),
+                    numero_baños = table.Column<int>(type: "int", nullable: false),
+                    capacidad_maxima = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Aviones", x => x.id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Aviones");
+        }
+    }
+}
